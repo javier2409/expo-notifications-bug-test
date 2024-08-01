1f188ec3aa4df140350fc94d44e2a62eb00d70be
Javier <javier.ferreyra@creativecoefficient.net>
2024-08-01 11:47:10 -0300

    Setup notifications

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,8 +4,26 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import * as Notifications from 'expo-notifications';
+import {useEffect} from "react";
 
 export default function HomeScreen() {
+
+  useEffect(() => {
+    Notifications.addPushTokenListener(({data, type}) => {
+      console.log('Push token', data, type);
+    });
+
+    Notifications.requestPermissionsAsync().then((status) => {
+      if (status.granted) {
+        Notifications.getExpoPushTokenAsync().then((token) => {
+          console.log('Expo push token', token);
+        });
+      }
+    })
+
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
